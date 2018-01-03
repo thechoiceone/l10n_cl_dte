7109f19962a698628a7c2ca574bb77dc1aa9d699
thechoiceone <cesarcardenas@puntotron.com>
2018-01-03 01:16:28 -0300

    importa dependencia
    importa  filetype pem

--- models/invoice.py
+++ models/invoice.py
@@ -451,7 +451,7 @@ version="1.0">
         from cryptography.hazmat.backends import default_backend
         from cryptography.hazmat.primitives.serialization import load_pem_private_key
         import OpenSSL
-        from OpenSSL.crypto import load_privatekey, sign
+        from OpenSSL.crypto import FILETYPE_PEM, load_privatekey, sign
         type_ = FILETYPE_PEM
         key=OpenSSL.crypto.load_privatekey(type_,privkey.encode('ascii'))
         signature= OpenSSL.crypto.sign(key,signed_info_c14n,'sha1')
